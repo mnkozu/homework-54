54882cb418af1fc769123a55dd07b0ac54ff6931
Nurkyz Mamatnazar <nurkyzmamatnazar@gmail.com>
2022-11-01 18:04:03 +0600

    Подготовила проект к работе. Изначальный стейт готов.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const createItems = () => {
+    const initialState = [];
+
+    for (let i = 0; i < 36; i++) {
+      initialState.push({hasItem: false, clicked: false})
+    }
+
+    const randomIndex = Math.floor(Math.random() * 36);
+
+    initialState[randomIndex].hasItem = true;
+
+    return initialState;
+  };
+
+  const [cells, setCells] = useState(createItems());
+  const [tries, setTries] = useState(0);
+
+  const openCell = (index: number) => {
+    console.log(index)
+    setCells(prev => prev.map((cell, i) => {
+      if (index === i) {
+        return {
+          ...cell,
+          hasItem: true,
+        };
+      }
+
+      return cell;
+    }))
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Board">
+        {cells.map((cell, index) =>(
+          <div
+            key={index}
+            className="Cell"
+            onClick={() => openCell(index)}
+          >
+          </div>
+        ))}
+      </div>
+
+      <div className="Counter">Tries: {tries}</div>
+      <div className="Controls">
+        <button type="button">Reset</button>
+      </div>
     </div>
   );
 }
